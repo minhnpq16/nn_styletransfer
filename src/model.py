3ac4fbbe7f9bde51ff4d3a7d6d9f8a0d923eaa81
minhnpq16 <minhnpq16@gmail.com>
2017-10-10 18:54:51 +0200

    original gatys' model with a few tweaks

--- src/model.py
+++ src/model.py
@@ -8,39 +8,46 @@ import atexit
 
 try:
 #Model arguments
-    np.random.seed(416)
-    tf.set_random_seed(256)
+    #np.random.seed(416)
+    #tf.set_random_seed(256)
 
-    target_size = (224, 224)
+    target_size = None #(224, 224)
+    load = False 
 
-    learn_rate = 0.1 
-    n_iter = 5000
+    learn_rate = 1.
+    n_iter = 1000
 
-    content_layer_name = 'block4_conv4'
+    content_layer_name = 'block4_conv2'
+    style_layer_names = [('block1_conv2', 1e1),
+                         ('block2_conv2', 1e1),
+                         ('block3_conv2', 5e-2),
+                         ('block4_conv2', 2e1),
+                         ('block5_conv2', 1.)]
     content_weight = 1.
-    style_layer_names = [('block1_conv1', 1.),
-                         ('block2_conv1', 1.),
-                         ('block3_conv1', 1.),
-                         ('block4_conv1', 1.),
-                         ('block5_conv1', 1.)]
-    style_weight = 1.
+    style_weight = 1e-5
+    totvar_weight = 0.
 
     sess = tf.Session()
     K.set_session(sess)
 
 
 #Load images
-    content_image_path = '../img/portrait.jpg'
+    content_image_path = '../img/city1.jpg'
     content_image = load_prep_image(content_image_path, target_size)
     style_image_path = '../img/style3.jpg'
     style_image = load_prep_image(style_image_path, content_image.shape[1:3])
-    whitenoise_image = np.random.normal(size=content_image.shape, scale=16)
+    whitenoise_image = np.random.random(content_image.shape) * 64. - 32.
+    noisy_content_image = content_image + whitenoise_image
 
 
 #Build model
-    X = tf.Variable(whitenoise_image, name='X')
+    if (load):
+        saved_image = np.load('saved_syn_image.npy')
+        X = tf.Variable(saved_image, name='X', dtype=tf.float32)
+    else:
+        X = tf.Variable(whitenoise_image, name='X', dtype=tf.float32)
     base_model = VGG19(input_tensor=X, include_top=False, weights='imagenet')
-#base_model.summary()
+#    base_model.summary()
 
 
 #Build content loss
@@ -61,21 +68,29 @@ try:
         M = shp[1] * shp[2]
         style_layer = tf.reshape(style_layer, (-1, M, N))
         trans_layer = tf.transpose(style_layer, (0, 2, 1))
-        gram_matrix = tf.matmul(trans_layer, style_layer) / tf.cast(M, tf.float32)
+        gram_matrix = tf.matmul(trans_layer, style_layer) / tf.cast(M*N, tf.float32)
         (style_target_arr, trans_target_arr, gram_target_arr) = \
                 sess.run([style_layer, trans_layer, gram_matrix], feed_dict={X: style_image})
 #        print(type(style_target_arr), style_target_arr.shape)
 #        print(type(trans_target_arr), trans_target_arr.shape)
 #        print(type(gram_target_arr), gram_target_arr.shape)
         gram_target = tf.Variable(gram_target_arr, name='gram_target')
-        layer_loss = tf.reduce_sum(tf.square(gram_matrix - gram_target)) / tf.cast(4 * N * N, tf.float32)
-        style_layer_losses.append(layer_loss)
+        layer_loss = tf.reduce_sum(tf.square(gram_matrix - gram_target)) #/ tf.cast(N * N, tf.float32)
+        style_layer_losses.append(layer_loss * layer_weight)
         style_loss = style_loss + layer_loss * layer_weight
 
 
+#Denoising
+    totvar_loss = tf.reduce_sum(tf.abs(X[:, 1:, :, :] - X[:, :-1, :, :])) + \
+                  tf.reduce_sum(tf.abs(X[:, :, 1:, :] - X[:, :, :-1, :]))
+#    totvar_loss = tf.cast(totvar_loss, tf.float32)
+
+
 #Model loss
-#loss = content_loss * content_weight 
-    loss = style_loss * style_weight
+    content_contrib = content_loss * content_weight
+    style_contrib = style_loss * style_weight
+    totvar_contrib = totvar_loss * totvar_weight
+    loss = totvar_contrib + content_contrib + style_contrib
 
 
 #Optimizing
@@ -85,13 +100,14 @@ try:
     t = trange(n_iter)
     for i in t:
         sess.run(adam_op)
-        all_losses = (loss,) + tuple(style_layer_losses)
+        all_losses = (loss, totvar_contrib, content_contrib, style_contrib) + tuple(style_layer_losses)
         current_loss = sess.run(all_losses)
-        t.set_description('Loss: %e %e %e %e %e %e' % (current_loss))
+        t.set_description('%e tv%e con%e sty%e %e %e %e %e %e' % (current_loss))
 
 finally:
 #Save image
     syn_image = sess.run(X)
+    np.save('saved_syn_image.npy', syn_image)
     save_deprep_image('../img/syn_portrait.jpg', syn_image)
         
 
